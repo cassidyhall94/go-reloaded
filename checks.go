27af0b644604dd1134564be8b0f0a44e6262253b
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2022-01-14 17:12:42 +0000

    go-reloaded

--- checks.go
+++ checks.go
@@ -2,6 +2,22 @@ package main
 
 import "strings"
 
+// how to nest functions
+//func main() {
+//	fmt.Println(mult(2, 3, 2, 4))
+//}
+//
+//func mult(a, b, c, d int) int {
+//	sum2 := add(c, d)
+//	return add(a, b) * sum2
+//}
+//
+//func add(e, f int) int {
+//	sum := e + f
+//	return sum
+//}
+
+
 // func to look for (hex) and trigger the hex function
 // loop through string and replace number before (hex) to dec instead using hex function
 
