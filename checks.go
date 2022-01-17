51792d144c8672dff4ef92d8440e7ff8e3d12da8
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2022-01-17 18:29:58 +0000

    go-reloaded

--- checks.go
+++ checks.go
@@ -1,28 +1,83 @@
 package main
 
-import "strings"
-
-// how to nest functions
-//func main() {
-//	fmt.Println(mult(2, 3, 2, 4))
-//}
-//
-//func mult(a, b, c, d int) int {
-//	sum2 := add(c, d)
-//	return add(a, b) * sum2
-//}
-//
-//func add(e, f int) int {
-//	sum := e + f
-//	return sum
-//}
-
+import (
+	"strings"
+)
 
 // func to look for (hex) and trigger the hex function
 // loop through string and replace number before (hex) to dec instead using hex function
 
-func checkhex(s string) string {
+func checkhex(s string) (string, error) {
+	splitS := strings.Split(s, " ")
+	hexvar := "(hex)"
+
+	for i := range splitS {
+		if len(splitS) == i+3 {
+			break
+		} else if splitS[i] == hexvar {
+			result, err := hex([]rune(splitS[i-1]))
+			if err != nil {
+				return "", err
+			}
+			splitS[i-1] = string(result)
+			splitS = removeIndex(splitS, i)
+		}
+	}
+	return strings.Join(splitS, " "), nil
+}
+
+func checkbin(s string) (string, error) {
+	splitS := strings.Split(s, " ")
+	binvar := "(bin)"
+
+	for i := range splitS {
+		if len(splitS) == i+3 {
+			break
+		} else if splitS[i] == binvar {
+			result, err := bin([]rune(splitS[i-1]))
+			if err != nil {
+				return "", err
+			}
+			splitS[i-1] = string(result)
+			splitS = removeIndex(splitS, i)
+		}
+	}
+	return strings.Join(splitS, " "), nil
+}
+
+func checkupper(s string) string {
 	splitS := strings.Split(s, " ")
 
+	for i := range splitS {
+		if i == 0 {
+			continue
+		} else if len(splitS) == i {
+			break
+		}
+		// need to add the period after TIMES for test 7: look for func in https://pkg.go.dev/strings#example-Contains library
+		if isLower(splitS[i-1]) && strings.Contains(splitS[i], "(up)") {
+			result := upper([]rune(splitS[i-1]))
+			splitS[i-1] = string(result)
+			splitS = removeIndex(splitS, i)
+		}
+	}
 	return strings.Join(splitS, " ")
+}
+
+func isLower(s string) bool {
+	counter := 0
+	for _, stringrune := range s {
+		if (stringrune >= rune(97)) && (stringrune <= rune(122)) {
+			counter++
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func removeIndex(s []string, index int) []string {
+	ret := make([]string, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
 }
--- checks_test.go
+++ checks_test.go
@@ -6,9 +6,191 @@ import (
 )
 
 func Test_checkhex(t *testing.T) {
-	test1 := checkhex("42 (hex)")
-	if !reflect.DeepEqual(test1, "66") {
+	test1, err := checkhex("42 (hex)")
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test1, "66") {
 		t.Logf("checkhex(\"42 (hex)\") failed, wanted \"%v\", got \"%v\"", "66", test1)
+		t.Fail()
+	}
+
+	test2, err := checkhex("42 (hox)")
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test2, "42 (hox)") {
+		t.Logf("checkhex(\"42 (hox)\") failed, wanted \"%v\", got \"%v\"", "42 (hox)", test2)
+		t.Fail()
+	}
+
+	test3, err := checkhex("42 ()")
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test3, "42 ()") {
+		t.Logf("checkhex(\"42 ()\") failed, wanted \"%v\", got \"%v\"", "42 ()", test3)
+		t.Fail()
+	}
+
+	test4, err := checkhex("42 hex")
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test4, "42 hex") {
+		t.Logf("checkhex(\"42 hex\") failed, wanted \"%v\", got \"%v\"", "42 (hex)", test4)
+		t.Fail()
+	}
+
+	test5, err := checkhex("xx (hex)")
+	if err == nil {
+		t.Logf("xx (hex) failed, wanted err, got test5 == %v", test5)
+		t.Fail()
+	}
+
+	test6, err := checkhex("1e (hex)")
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test6, "30") {
+		t.Logf("checkhex(\"1e (hex)\") failed, wanted \"%v\", got \"%v\"", "30", test6)
+		t.Fail()
+	}
+
+	test7In := "Simply add 42 (hex) and 10 (up) and you will see the result is 68"
+	test7Want := "Simply add 66 and 10 (up) and you will see the result is 68"
+	test7, err := checkhex(test7In)
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test7, test7Want) {
+		t.Logf("checkhex(\"%s\") failed, wanted \"%s\", got \"%s\"", test7In, test7Want, test7)
+		t.Fail()
+	}
+
+	test8In := "Simply add 42 (hex) and 10 (up) and you 1e (hex) will see the result is 68"
+	test8Want := "Simply add 66 and 10 (up) and you 30 will see the result is 68"
+	test8, err := checkhex(test8In)
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test8, test8Want) {
+		t.Logf("checkhex(\"%s\") failed, wanted \"%s\", got \"%s\"", test8In, test8Want, test8)
+		t.Fail()
+	}
+}
+
+func Test_checkbin(t *testing.T) {
+	test1, err := checkbin("10 (bin)")
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test1, "2") {
+		t.Logf("checkbin(\"10 (bin)\") failed, wanted \"%v\", got \"%v\"", "42", test1)
+		t.Fail()
+	}
+
+	test2, err := checkbin("42 (hox)")
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test2, "42 (hox)") {
+		t.Logf("checkbin(\"42 (hox)\") failed, wanted \"%v\", got \"%v\"", "42 (hox)", test2)
+		t.Fail()
+	}
+
+	test3, err := checkbin("42 ()")
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test3, "42 ()") {
+		t.Logf("checkbin(\"42 ()\") failed, wanted \"%v\", got \"%v\"", "42 ()", test3)
+		t.Fail()
+	}
+
+	test4, err := checkbin("42 bin")
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test4, "42 bin") {
+		t.Logf("checkbin(\"42 bin\") failed, wanted \"%v\", got \"%v\"", "42 (bin)", test4)
+		t.Fail()
+	}
+
+	test5, err := checkbin("xx (bin)")
+	if err == nil {
+		t.Logf("xx (bin) failed, wanted err, got test5 == %v", test5)
+		t.Fail()
+	}
+
+	test6, err := checkbin("110 (bin)")
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test6, "6") {
+		t.Logf("checkbin(\"110 (bin)\") failed, wanted \"%v\", got \"%v\"", "6", test6)
+		t.Fail()
+	}
+
+	test7In := "Simply add 10 (bin) and 66 you will see the result is 68"
+	test7Want := "Simply add 2 and 66 you will see the result is 68"
+	test7, err := checkbin(test7In)
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test7, test7Want) {
+		t.Logf("checkbin(\"%s\") failed, wanted \"%s\", got \"%s\"", test7In, test7Want, test7)
+		t.Fail()
+	}
+
+	test8In := "Simply add 100110 (bin) and 11110 (bin) and you will see the result is 68"
+	test8Want := "Simply add 38 and 30 and you will see the result is 68"
+	test8, err := checkbin(test8In)
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	} else if !reflect.DeepEqual(test8, test8Want) {
+		t.Logf("checkbin(\"%s\") failed, wanted \"%s\", got \"%s\"", test8In, test8Want, test8)
+		t.Fail()
+	}
+}
+
+func Test_checkupper(t *testing.T) {
+	// test1 := checkupper("hey (up)")
+	// if !reflect.DeepEqual(test1, "HEY") {
+	// 	t.Logf("checkupper(\"hey (up)\") failed, wanted \"%v\", got \"%v\"", "HEY", test1)
+	// 	t.Fail()
+	// }
+
+	// test2 := checkupper("Ready, set, go (up) !")
+	// if !reflect.DeepEqual(test2, "Ready, set, GO !") {
+	// 	t.Logf("checkupper(\"Ready, set, go (up) !\") failed, wanted \"%v\", got \"%v\"", "Ready, set, GO !", test2)
+	// 	t.Fail()
+	// }
+
+	// test3 := checkupper("42 (up)")
+	// if !reflect.DeepEqual(test3, "42 (up)") {
+	// 	t.Logf("checkupper(\"42 (up)\") failed, wanted \"%v\", got \"%v\"", "42 (up)", test3)
+	// 	t.Fail()
+	// }
+
+	// test4 := checkupper("hey up")
+	// if !reflect.DeepEqual(test4, "hey up") {
+	// 	t.Logf("checkupper(\"hey up\") failed, wanted \"%v\", got \"%v\"", "hey up", test4)
+	// 	t.Fail()
+	// }
+
+	// test6 := checkupper("HEY (up)")
+	// if !reflect.DeepEqual(test6, "HEY (up)") {
+	// 	t.Logf("checkupper(\"HEY (up)\") failed, wanted \"%v\", got \"%v\"", "Hey (up)", test6)
+	// 	t.Fail()
+	// }
+
+	test7In := "it was the best of times, it was the worst of times (up)."
+	test7Want := "it was the best of times, it was the worst of TIMES."
+	test7 := checkupper(test7In)
+	if !reflect.DeepEqual(test7, test7Want) {
+		t.Logf("checkupper(\"%s\") failed, wanted \"%s\", got \"%s\"", test7In, test7Want, test7)
 		t.Fail()
 	}
 }
